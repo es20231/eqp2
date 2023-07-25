d79ed68791d78d515a3baf75cab365527e1c09fd
João Pedro <joao2015.76@gmail.com>
2023-07-25 01:00:37 -0300

    Commit do teste unitario de cadastro de usuario

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
-from .forms import PostForm
 from .models import Post
 from django.contrib.auth.decorators import login_required
 
@@ -8,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 def publicar(request):
     """Função que publica um post do usuário"""
     if request.method =="GET":
-        return render(request, 'novo-post.html')
+        return render(request, 'post/novo-post.html')
     
     else:
         id_post = request.POST.get('id')
@@ -27,7 +26,7 @@ def publicar(request):
         novo_post.data_de_publicacao = data_de_publicacao_post
         novo_post.save()
 
-        return render(request, 'detalhes-post.html')
+        return render(request, 'post/detalhes-post.html')
 
 @login_required(login_url='/autenticacao/login/')   
 def remover(request, id):
