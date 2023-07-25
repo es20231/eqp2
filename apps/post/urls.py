17b517334ff6d18869a39284f08b6d7e3a703a2b
Ciro Melo <ciroolmelo@ufpi.edu.br>
2023-07-25 01:20:25 -0300

    adição dos arquivos de teste

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('publicar/', views.publicar),
     path('<uuid:id>/remover/', views.remover),
     path('<uuid:id>/visualizar/', views.visualizar)
 ]
