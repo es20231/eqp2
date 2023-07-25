cc703e1c167cbc2d44dd2d6da41839b43b1be565
João Pedro <joao2015.76@gmail.com>
2023-07-24 21:35:18 -0300

    Commit signals para criação do profile e urls

---
+++ apps/autenticacao/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save #Import a post_save signal when a user is created
+from django.contrib.auth.models import User # Import the built-in User model, which is a sender
+from django.dispatch import receiver # Import the receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User) # The receiver decorator takes the signal and the sender as arguments
+def create_profile(sender, instance, created, **kwargs): # The receiver function takes the arguments of the signal
+    if created: # If the user is created
+        Profile.objects.create(usuario=instance) # Create a profile object with the user instance
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from autenticacao.forms import ProfileForm
+from django.contrib import messages
 
 @login_required(login_url='/autenticacao/login/')
 def dashboard(request):
-    usuario = request.user.get_full_name()
-    return HttpResponse(f'Bem vindo, {usuario}!')
-    return render(request, 'dashboard/dashboard.html')
+    """Função que renderiza a página inicial do dashboard"""
+
+    if request.method == "POST":
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Perfil atualizado com sucesso')
+            return redirect(dashboard)
+        
+    else:
+        profile_form = ProfileForm(instance=request.user.profile)
+
+    return render(request, 'dashboard/dash.html', {'profile_form': profile_form})
